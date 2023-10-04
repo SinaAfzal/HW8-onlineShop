36d94b1e6494d990617367002d0bbf82ee2d3f24
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-10-04 17:53:02 +0330

    create and develop CartService interface

---
+++ src/main/java/ir/maktabsharif101/hw8/service/CartService.java
@@ -0,0 +1,14 @@
+package ir.maktabsharif101.hw8.service;
+
+import ir.maktabsharif101.hw8.base.service.BaseService;
+import ir.maktabsharif101.hw8.entity.Cart;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CartService extends BaseService<Integer, Cart> {
+    List<Cart> listAllInCart(Integer user_id) throws SQLException;
+    boolean hasItemsInCart(Integer user_id) throws SQLException;
+    int countOfItemInCart(Integer item_id) throws SQLException;
+    String nameOfItemInCart(Integer item_id) throws SQLException;
+}
