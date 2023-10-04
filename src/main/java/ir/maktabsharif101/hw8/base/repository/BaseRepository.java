de6f9f071cb3e51a51586bbeb78d6323431be108
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-10-04 17:49:23 +0330

    create and develop BaseRepository interface

---
+++ src/main/java/ir/maktabsharif101/hw8/base/repository/BaseRepository.java
@@ -0,0 +1,14 @@
+package ir.maktabsharif101.hw8.base.repository;
+
+import ir.maktabsharif101.hw8.base.entity.BaseEntity;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+
+public interface BaseRepository<ID extends Serializable,Type extends BaseEntity<ID>> {
+    Type save(Type entity) throws SQLException;
+    void delete(ID id) throws SQLException;
+    int update(Type entity) throws SQLException;
+    boolean doesExist(ID id) throws SQLException;
+
+}
