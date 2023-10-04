5004f643dba3a54a3a2d06b0911c87929239b5dc
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-10-04 17:48:22 +0330

    implement methods in BaseEntity class

--- src/main/java/ir/maktabsharif101/hw8/base/entity/BaseEntity.java
+++ src/main/java/ir/maktabsharif101/hw8/base/entity/BaseEntity.java
@@ -5,6 +5,13 @@ import java.io.Serializable;
 public class BaseEntity<ID extends Serializable> {
     private ID id;
 
+    public BaseEntity() {
+    }
+
+    public BaseEntity(ID id) {
+        this.id = id;
+    }
+
     public ID getId() {
         return id;
     }
