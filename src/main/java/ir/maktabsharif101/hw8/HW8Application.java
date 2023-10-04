ee3bf794545709a371aede641539e25acab9697c
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-10-04 17:55:47 +0330

    create and develop HW8Application class (Main class)

--- src/main/java/ir/maktabsharif101/hw8/HW8Application.java
+++ src/main/java/ir/maktabsharif101/hw8/HW8Application.java
@@ -1,7 +1,17 @@
 package ir.maktabsharif101.hw8;
 
+import ir.maktabsharif101.hw8.entity.enums.Electronics;
+import ir.maktabsharif101.hw8.utility.ApplicationContext;
+import ir.maktabsharif101.hw8.utility.ZereshkThrower;
+
+import java.sql.SQLException;
+
 public class HW8Application {
     public static void main(String[] args) {
-
+        try {
+            ApplicationContext.getHomePageMenu().HomePage();
+        }catch (ZereshkThrower | SQLException z){
+            System.out.println(z.getMessage());
+        }
     }
 }
